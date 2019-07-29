678e03e5420b1868662eceb61ec6d86f9eed2fe6
Zaikin Sergey <zaikinsr@yandex.ru>
2019-07-29 22:10:22 +0500

    Add configurable options IServiceCollection extension

---
+++ BlazorDateRangePicker/DateRangePickerExtensions.cs
@@ -0,0 +1,39 @@
+﻿/**
+* @author: Sergey Zaikin zaikinsr@yandex.ru
+* @copyright: Copyright (c) 2019 Sergey Zaikin. All rights reserved.
+* @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
+*/
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorDateRangePicker
+{
+    public static class DateRangePickerExtensions
+    {
+        /// <summary>
+        /// Adds a singleton <see cref="DateRangePickerConfig"/> instance to the DI
+        /// </summary>
+        public static IServiceCollection AddDateRangePicker(this IServiceCollection services, DateRangePickerConfig configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            services.AddSingleton(configuration);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds a singleton <see cref="DateRangePickerConfig"/> instance to the DI
+        /// </summary>
+        public static IServiceCollection AddDateRangePicker(this IServiceCollection services,
+            Action<DateRangePickerConfig> configure)
+        {
+            if (configure == null) throw new ArgumentNullException(nameof(configure));
+
+            var options = new DateRangePickerConfig();
+            configure(options);
+
+            return AddDateRangePicker(services, options);
+        }
+    }
+}
---
+++ BlazorDateRangePicker/IConfigurableOptions.cs
@@ -0,0 +1,47 @@
+﻿/**
+* @author: Sergey Zaikin zaikinsr@yandex.ru
+* @copyright: Copyright (c) 2019 Sergey Zaikin. All rights reserved.
+* @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace BlazorDateRangePicker
+{
+    internal interface IConfigurableOptions
+    {
+        bool AlwaysShowCalendars { get; set; }
+        string ApplyButtonClasses { get; set; }
+        string ApplyLabel { get; set; }
+        Dictionary<string, object> Attributes { get; set; }
+        bool AutoApply { get; set; }
+        string ButtonClasses { get; set; }
+        string CancelButtonClasses { get; set; }
+        string CancelLabel { get; set; }
+        CultureInfo Culture { get; set; }
+        string CustomDateClass { get; set; }
+        Func<DateTimeOffset, bool> CustomDateFunction { get; set; }
+        string CustomRangeLabel { get; set; }
+        string DateFormat { get; set; }
+        Func<DateTimeOffset, bool> DaysEnabledFunction { get; set; }
+        DropsType Drops { get; set; }
+        DateTimeOffset? EndDate { get; set; }
+        DayOfWeek FirstDayOfWeek { get; set; }
+        bool LinkedCalendars { get; set; }
+        DateTimeOffset? MaxDate { get; set; }
+        TimeSpan? MaxSpan { get; set; }
+        DateTimeOffset? MinDate { get; set; }
+        SideType Opens { get; set; }
+        List<RangeItem> Ranges { get; set; }
+        bool ShowCustomRangeLabel { get; set; }
+        bool ShowDropdowns { get; set; }
+        bool ShowISOWeekNumbers { get; set; }
+        bool ShowWeekNumbers { get; set; }
+        bool SingleDatePicker { get; set; }
+        DateTimeOffset? StartDate { get; set; }
+        bool Visible { get; set; }
+        string WeekAbbreviation { get; set; }
+    }
+}
